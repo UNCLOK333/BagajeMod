ffdbc077290355fb5cf50800335167af02471b13
UNCLOK333 <117139666+UNCLOK333@users.noreply.github.com>
2025-02-16 18:52:14 -0600

    Add files via upload

---
+++ BagajeMod/Common/Systems/DarkOverlaySystem.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace BagajeMod.Content.Systems
+{
+    public class DarkOverlaySystem : ModSystem
+    {
+        public static float DarkenIntensity = 0f; // 0 = transparente, 1 = completamente oscuro
+        private Texture2D overlayTexture;
+
+        public override void Load()
+        {
+            // Cargar la textura cuando se inicializa el mod
+            overlayTexture = ModContent.Request<Texture2D>("BagajeMod/Assets/Textures/negro").Value;
+        }
+
+        public override void Unload()
+        {
+            // Liberar la referencia a la textura cuando se cierre el mod
+            overlayTexture = null;
+        }
+
+        public override void PostDrawInterface(SpriteBatch spriteBatch)
+        {
+            // Asegurar que el overlay solo se dibuje si la intensidad es mayor que 0
+            if (DarkenIntensity > 0f && overlayTexture != null)
+            {
+                spriteBatch.Draw(
+                    overlayTexture,
+                    new Rectangle(0, 0, Main.screenWidth, Main.screenHeight),
+                    Color.White * DarkenIntensity // Color.White para mantener la textura original
+                );
+            }
+        }
+    }
+}
---
+++ BagajeMod/Common/Systems/DownedBossSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using BagajeMod.Content.NPCs.Terry;
+
+namespace BagajeMod.Common.Systems
+{
+    public class DownedBossSystem : ModSystem
+    {
+        public static bool downedTerryBoss = false;
+
+        public override void OnWorldLoad()
+        {
+            downedTerryBoss = false;
+        }
+
+        public override void OnWorldUnload()
+        {
+            downedTerryBoss = false;
+        }
+
+        public override void SaveWorldData(TagCompound tag)
+        {
+            if (downedTerryBoss)
+            {
+                tag["downedTerryBoss"] = true;
+            }
+        }
+
+        public override void LoadWorldData(TagCompound tag)
+        {
+            downedTerryBoss = tag.ContainsKey("downedTerryBoss");
+        }
+    }
+}
